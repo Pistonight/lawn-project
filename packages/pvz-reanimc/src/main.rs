82a9e9e256d853476f374fc7907416320f272065
Michael Zhao <44533763+Pistonight@users.noreply.github.com>
2026-06-13 20:45:37 -0700

    moved tools to its own repo

--- packages/pvz-reanimc/src/main.rs
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (c) 2026 Pistonight/lawn-project contributors
-
-#[cu::cli]
-fn main(cli: pvz_reanimc::Cli) -> cu::Result<()> {
-    pvz_reanimc::run(cli)
-}
--- packages/pvz-utils/src/lib.rs
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2026 Pistonight/lawn-project contributors
-
-mod stream;
-pub use stream::*;
